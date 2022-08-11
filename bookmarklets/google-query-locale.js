646c2d0a1cfc0592fe7adfb46099373edb9d30c8
Rich Tatum <rich.tatum@gmail.com>
2022-08-11 09:05:49 -0400

    Update google-query-locale.js
    Added note about bookmarklet title

--- bookmarklets/google-query-locale.js
+++ bookmarklets/google-query-locale.js
@@ -17,8 +17,9 @@ javascript:(function(){
 
 
 /* Single Line Version */
-/* Create a bookmarklet by creating any bookmarklet anywhere you wish */
+/* Create a bookmarklet by creating any bookmarklet anywhere you wish. */
 /* Then edit the properties of the bookmarklet. Change the title to something sensible and useful */
+/* E.g.: 📍 Google Query Locale
 /* Then paste the following line into the location/URL field */
 /* If you want to link to it from HTML, wrap it in <a href="PASTEHERE">anchor</a> where PASTEHERE is the code below */
 javascript:(function(){if(location.hostname.indexOf('google.com')>-1){var localeQuery=prompt('Change locale to:','');location.href=window.location.toString().replace(/&near=[^&]*/,'')+'&near='+localeQuery;}else{var searchQuery=prompt('Search query:',''),localeQuery=prompt('Search locale:','');location.href='https://www.google.com/search?q=%27+searchQuery+%27&num=10%27+%27&near=%27+localeQuery;}}());

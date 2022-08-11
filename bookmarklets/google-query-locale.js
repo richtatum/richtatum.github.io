6b0502fe087053744dc5e7f29e69b2251dd99283
Rich Tatum <rich.tatum@gmail.com>
2022-08-11 09:00:22 -0400

    Update google-query-locale.js
    Added comments.

--- bookmarklets/google-query-locale.js
+++ bookmarklets/google-query-locale.js
@@ -1,1 +1,20 @@
-javascript:(function(){if(location.hostname.indexOf('google.com')>-1){var localeQuery=prompt('Change%20locale%20to:','');location.href=window.location.toString().replace(/&near=[^&]*/,'')+'&near='+localeQuery;}else{var searchQuery=prompt('Search%20query:',''),localeQuery=prompt('Search%20locale:','');location.href='https://www.google.com/search?q='+searchQuery+'&num=10'+'&near='+localeQuery;}}());
+javascript:(function(){
+   /* If the current domain is Google */
+   if (location.hostname.indexOf('google.com')>-1){
+   /* Set variable using a prompt for a city name */
+   var localeQuery=prompt('Change locale to:','');
+   /* Delete any existing &near= values, update the current URL with new locale */
+   location.href=window.location.toString().replace(/&near=[^&]*/,'')+'&near='+localeQuery;}
+   /* If the current domain is NOT Google */
+      else{
+      /* Set variable using a prompt for the query */
+   var searchQuery=prompt('Search query:',''),
+   /* Set variable using a prompt for the city name */
+   localeQuery=prompt('Search locale:','');
+   /* Send query to Google */
+   location.href='https://www.google.com/search?q='+searchQuery+'&num=10'+'&near='+localeQuery;}
+}());
+
+
+/* Single Line Version */
+javascript:(function(){if(location.hostname.indexOf('google.com')>-1){var localeQuery=prompt('Change locale to:','');location.href=window.location.toString().replace(/&near=[^&]*/,'')+'&near='+localeQuery;}else{var searchQuery=prompt('Search query:',''),localeQuery=prompt('Search locale:','');location.href='https://www.google.com/search?q=%27+searchQuery+%27&num=10%27+%27&near=%27+localeQuery;}}());

2426a613a2c460af3d6bedb1a520f921014f207c
Rich Tatum <rich.tatum@gmail.com>
2022-08-11 05:58:17 -0400

    Create google-query-locale.js
    Bookmarklet to query Google using the near locale parameter.

---
+++ bookmarklets/google-query-locale.js
@@ -0,0 +1,1 @@
+javascript:(function(){var searchQuery=prompt('Search query:',''),localeQuery=prompt('Search locale:','');window.location.href='https://www.google.com/search?q='+encodeURIComponent(searchQuery)+'&near='+encodeURIComponent(localeQuery)})();

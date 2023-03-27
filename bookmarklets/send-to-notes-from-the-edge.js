c1a9e5e8eb035d938fb49dadd24256dccf002130
RichTatum <rich.tatum@gmail.com>
2023-03-27 08:03:55 -0400

    Create send-to-notes-from-the-edge.js
    Original Save

---
+++ bookmarklets/send-to-notes-from-the-edge.js
@@ -0,0 +1,26 @@
+﻿// SEND TO NOTES FROM THE EDGE
+// Bookmarklet to capture URLs, titles, and any selected text to a Google Sheets form
+// Intended for use for capturing news items for Edgy Labs’ Newsletter: SEO Notes from the Edge
+// Created: 03/27/2023 08:02 AM
+// Updated: 03/27/2023 08:02 AM
+	// Saved to GitHub
+
+var domain = location.hostname.toString().replace(/(www\.|\.com|\.net|\.org)/g,'').replace(/^\w/, c => c.toUpperCase());
+var utmPattern = /(\?|&)(utm_[^&=]+=[^&]*)/gi;
+var wUrl = encodeURIComponent(location.href.replace(utmPattern, ''));
+var wTitle=encodeURIComponent(document.title + " | " + domain);
+var wDescription = document.querySelector('meta[name="description"]');
+var wSelection=encodeURIComponent(window.getSelection().toString().replace(/\s+/g,' ').trim());
+var formId="1FAIpQLScdXhUEd6FrkzSRgz3zakxyAquPDZ6ZiK_41_19JUxA0cd81A";
+var formUrl="https://docs.google.com/forms/d/e/"+formId+"/viewform";
+var data={
+	"entry.623797088":wTitle,
+	"entry.2082543780":wUrl,
+	"entry.1564268661":wSelection
+};
+if (wDescription !== null) {data["entry.1626307794"] = wDescription.getAttribute("content");};
+var params=Object.keys(data).map(function(key){return key+"="+data[key];}).join("&");
+var url=formUrl+"?"+params;
+var newWin=window.open("","Bookmarker");
+newWin.document.write("<script>window.stop();</script>");
+newWin.location.href=url;

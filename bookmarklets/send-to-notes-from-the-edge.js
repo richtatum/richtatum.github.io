ba16d60407b29fd3e0614a533c5505d267216862
RichTatum <rich.tatum@gmail.com>
2023-04-04 11:14:48 -0400

    Update send-to-notes-from-the-edge.js
       Modified comment braces
       Provided fallback for meta description
       Changed var to const
       Added whitespace and trim to variables
       Employed template literals

--- bookmarklets/send-to-notes-from-the-edge.js
+++ bookmarklets/send-to-notes-from-the-edge.js
@@ -1,28 +1,42 @@
-﻿// SEND TO NOTES FROM THE EDGE
-// Bookmarklet to capture URLs, titles, and any selected text to a Google Sheets form
-// Intended for use for capturing news items for Edgy Labs’ Newsletter: SEO Notes from the Edge
-// Created: 03/27/2023 08:02 AM
-// Updated: 03/27/2023 8:22 AM
-	// Saved to GitHub
-	// Removed TLD deletion
-
-// var domain = location.hostname.toString().replace(/(www\.|\.com|\.net|\.org)/g,'').replace(/^\w/, c => c.toUpperCase());
-var domain = location.hostname.toString().replace(/^\w/, c => c.toUpperCase());
-var utmPattern = /(\?|&)(utm_[^&=]+=[^&]*)/gi;
-var wUrl = encodeURIComponent(location.href.replace(utmPattern, ''));
-var wTitle=encodeURIComponent(document.title + " | " + domain);
-var wDescription = document.querySelector('meta[name="description"]');
-var wSelection=encodeURIComponent(window.getSelection().toString().replace(/\s+/g,' ').trim());
-var formId="1FAIpQLScdXhUEd6FrkzSRgz3zakxyAquPDZ6ZiK_41_19JUxA0cd81A";
-var formUrl="https://docs.google.com/forms/d/e/"+formId+"/viewform";
-var data={
-	"entry.623797088":wTitle,
-	"entry.2082543780":wUrl,
-	"entry.1564268661":wSelection
-};
-if (wDescription !== null) {data["entry.1626307794"] = wDescription.getAttribute("content");};
-var params=Object.keys(data).map(function(key){return key+"="+data[key];}).join("&");
-var url=formUrl+"?"+params;
-var newWin=window.open("","Bookmarker");
-newWin.document.write("<script>window.stop();</script>");
-newWin.location.href=url;
+﻿/*
+SEND TO NOTES FROM THE EDGE
+Bookmarklet to capture URLs, titles, and any selected text to a Google Sheets form
+Intended for use for capturing news items for Edgy Labs' Newsletter: SEO Notes from the Edge
+Created: 03/27/2023 08:02 AM
+Updated: 04/04/2023 10:10 AM
+   Modified comment braces
+   Provided fallback for meta description
+   Changed var to const
+   Added whitespace and trim to variables
+   Employed template literals
+Updated: 03/27/2023 8:22 AM
+   Saved to GitHub
+   Removed TLD deletion
+*/
+function sendToNotesFromTheEdge(){
+   const domain=location.hostname.toString().replace(/(www\.|\.com|\.net|\.org)/g,'').replace(/^\w/,c=> c.toUpperCase());
+   const utmPattern=/(\?|&)(utm_[^&=]+=[^&]*)/gi;
+   const wUrl=encodeURI(location.href.replace(utmPattern,''));
+   const wTitle=`${document.title} | ${domain}`;
+   const formID='1FAIpQLScdXhUEd6FrkzSRgz3zakxyAquPDZ6ZiK_41_19JUxA0cd81A';
+   const formUrl='https://docs.google.com/forms/d/e/' + formID + '/viewform';
+   let description='';
+   const wDescription=document.querySelector('meta[name="description"],meta[itemprop="description"]');
+   if(wDescription){description=wDescription.content.replace(/\s+/g,' ').trim();}
+   else{
+      const ogDescription=document.querySelector('meta[property="og:description"]').replace(/\s+/g,' ').trim();
+      const twitterDescription=document.querySelector('meta[name="twitter:description"]').replace(/\s+/g,' ').trim();
+      if(ogDescription){description=ogDescription.content.replace(/\s+/g,' ').trim();}
+      else if(twitterDescription){description=twitterDescription.content.replace(/\s+/g,' ').trim();}
+      }
+   const wSelection=document.getSelection().toString().replace(/\s+/g,' ').trim();
+   const form=document.getElementById('notes-from-the-edge-form');
+   const params=new URLSearchParams();
+   params.append('entry.623797088',wTitle);
+   params.append('entry.2082543780',wUrl);
+   params.append('entry.1564268661',wSelection);
+   if(description!==""){params.append('entry.1626307794', description);}
+   const url=`${formUrl}?${params.toString()}`;
+   /* console.log(url); */
+   window.location.replace(url)
+}sendToNotesFromTheEdge();
